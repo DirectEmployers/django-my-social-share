18eb7160a24ce62d90b2abcbae08a2ad16b040ed
Mike Seidle <mike@directemployers.org>
2011-12-13 14:43:27 -0500

    add admin.py

--- myshare/myshare/admin.py
+++ myshare/myshare/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from models import Network, Share
+
+class NetworkOption(admin.ModelAdmin):
+    """Default Network admin defs"""
+    list_display=('name','short_name')
+    
+class ShareOption(admin.ModelAdmin):
+    """Default share history admin defs"""
+    list_display=('created', 'user', 'network.short_name')
