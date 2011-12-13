a03c6248b58f3e96f11b854f6cea42d46b0e8385
Mike Seidle <mike@directemployers.org>
2011-12-13 14:06:03 -0500

    start urls.py

--- myshare/myshare/urls.py
+++ myshare/myshare/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'views.index', name='index'),
+    url(r'^page$', 'page', name='page')
+    
+    
 )
--- myshare/myshare/views.py
+++ myshare/myshare/views.py
@@ -6,8 +6,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404
 from django.template import RequestContext
 from django.utils.translation import ugettext, ugettext_lazy as _
-    
-    
+
 
 def index(request):
     """Implements home page view"""
