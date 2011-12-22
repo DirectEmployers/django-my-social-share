656c851ba711f02582240d56df93788396ea1c3b
Mike Seidle <mike@directemploers.org>
2011-12-22 11:35:39 -0500

    Define modelforms for shares

--- myshare/myshare/forms.py
+++ myshare/myshare/forms.py
@@ -1,1 +1,32 @@
+"""
+forms.py -- implements forms for my social share
+"""
+
 from django import forms
+from django.forms import ModelForm
+from myshare.models import Network, Share
+
+class EmailSaveFormAuthenticated(ModelForm):
+    """Implements email form where user can customize the message
+    
+    Allows user to choose to save what is being shared in their personal history
+    as well."""
+    
+    class Meta:
+        Model = Share
+
+class DirectShareForm(ModelForm):
+    """Implements direct (api) sharing form
+    
+    This form allows the user to enter a share and select one or more networks
+    to share to. Requires django-socialauth
+    """
+    
+    class Meta:
+        Model = Share
+
+class EmailSaveFormNotAuthenticated(ModelForm):
+    """Implements anonymous email form without user customization"""
+    
+    class Meta:
+        Model = Share
